aba10be503455ed958c713513ce4ba583af960ed
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2021-02-25 16:41:16 +0100

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -21,92 +21,27 @@ void Player::keyPressEvent(QKeyEvent *event)
 void Player::keyReleaseEvent(QKeyEvent *event)
 {
     keys[event->key()] = false;
-    movePlayer();
 }
 
 void Player::movePlayer()
 {
-    /*if(event->key() == Qt::Key_D)
-    {
-        if(keys[90] == true) // key Z
-        {
-            setPos(x()+10, y()-10);
-        }
-        else if(keys[83] == true) // key S
-        {
-            setPos(x()+10, y()+10);
-        }
-        else
-        {
-            setPos(x()+10, y());
-        }
-    }
-
-    if(event->key() == Qt::Key_Q)
-    {
-        if(keys[90] == true) // key Z
-        {
-            setPos(x()-10, y()-10);
-        }
-        else if(keys[83] == true) // key S
-        {
-            setPos(x()-10, y()+10);
-        }
-        else
-        {
-            setPos(x()-10, y());
-        }
-    }
-
-    if(event->key() == Qt::Key_Z)
-    {
-        if(keys[68] == true) // key D
-        {
-            setPos(x()+10, y()-10);
-        }
-        else if(keys[81] == true) // key Q
-        {
-            setPos(x()-10, y()-10);
-        }
-        else
-        {
-            setPos(x(), y()-10);
-        }
-    }
-
-    if(event->key() == Qt::Key_S)
-    {
-        if(keys[68] == true) // key D
-        {
-            setPos(x()+10, y()+10);
-        }
-        else if(keys[81] == true) // key Q
-        {
-            setPos(x()-10, y()+10);
-        }
-        else
-        {
-            setPos(x(), y()+10);
-        }
-    }*/
-
     if(keys[68] == true) // key D
     {
-        setPos(x()+10, y());
+        setPos(x()+20, y());
     }
 
     if(keys[81] == true) // key Q
     {
-        setPos(x()-10, y());
+        setPos(x()-20, y());
     }
 
     if(keys[90] == true) // key Z
     {
-        setPos(x(), y()-10);
+        setPos(x(), y()-20);
     }
 
     if(keys[83] == true) // key S
     {
-        setPos(x(), y()+10);
+        setPos(x(), y()+20);
     }
 }
