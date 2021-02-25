ef002650c3546ffb8e36b3687d3b11648c7f20df
HDSean <sean.anica@etu.univ-tours.fr>
2021-02-25 17:33:52 +0100

    branch setup

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-#include "widget.h"
-
 #include <QApplication>
+#include "game.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    Game *myGame = new Game();
+    myGame->startGame();
 
     return a.exec();
 }
