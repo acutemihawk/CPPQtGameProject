19e0620111203b49b22a3397e8c56253fbd0bd9d
Alexandre1408 <75449690+Alexandre1408@users.noreply.github.com>
2021-02-27 21:45:48 +0100

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,52 +1,25 @@
-#include "player.h"
+#ifndef PLAYER_H
+#define PLAYER_H
 
-Player::Player()
+#include <QGraphicsPixmapItem>
+#include <QKeyEvent>
+#include <QMap>
+#include "entity.h"
+
+class Player: public QGraphicsPixmapItem
 {
-    setPixmap(QPixmap(":/sprites/devant1.png"));
-    setFlag(QGraphicsItem::ItemIsFocusable);
-    setFocus();
-}
+    public:
+        Player();
+        ~Player();
+        void keyPressEvent(QKeyEvent * event);
+        void keyReleaseEvent(QKeyEvent *event);
+        void movePlayer();
+        bool isColliding();
+    private:
+        int xCollision;
+        int yCollision;
+        QMap<int, bool> keys;
+        QMap<int, bool> directionColliding;
+};
 
-Player::~Player()
-{
-
-}
-
-void Player::keyPressEvent(QKeyEvent *event)
-{
-    keys[event->key()] = true;
-    movePlayer();
-}
-
-void Player::keyReleaseEvent(QKeyEvent *event)
-{
-    keys[event->key()] = false;
-}
-
-void Player::movePlayer()
-{
-    if(keys[68] == true) // key D
-    {
-        setPixmap(QPixmap(":/sprites/droite1.png"));
-        setPos(x()+10, y());
-    }
-
-    if(keys[81] == true) // key Q
-    {
-        setPixmap(QPixmap(":/sprites/gauche1.png"));
-        setPos(x()-10, y());
-    }
-
-    if(keys[90] == true) // key Z
-    {
-        setPixmap(QPixmap(":/sprites/derriere1.png"));
-        setPos(x(), y()-10);
-    }
-
-    if(keys[83] == true) // key S
-    {
-        setPixmap(QPixmap(":/sprites/devant1.png"));
-        setPos(x(), y()+10);
-    }
-
-}
+#endif // PLAYER_H
