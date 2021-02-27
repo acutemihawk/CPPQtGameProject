70267433036913278f4df4685721813f0c1d36ec
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2021-02-27 23:14:46 +0100

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -18,11 +18,15 @@ void Game::startGame()
     view->setBackgroundBrush(QBrush(QImage(":/sprites/bg.png")));
     view->setScene(scene);
     view->setWindowTitle("Zeldo");
-    view->setFixedSize(1300, 990);
+    view->setFixedSize(600, 600);
     view->show();
 
     Player *player = new Player();
     Entity *entity1 = new Entity("un premier test", 100, 100, QPixmap(":/sprites/tree.png"));
+    entity1->boundingRect().setBottom(entity1->getY()+50);
+    entity1->boundingRect().setTop(entity1->getY()-50);
+    entity1->boundingRect().setRight(entity1->getX()+50);
+    entity1->boundingRect().setLeft(entity1->getX()-50);
     scene->addItem(entity1);
 
     scene->addItem(player);
