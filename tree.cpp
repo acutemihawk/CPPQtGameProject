86cafd15f9acac97659857d67996e2182e547501
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2021-03-03 21:47:02 +0100

    Create tree.cpp

---
+++ tree.cpp
@@ -0,0 +1,20 @@
+#include "tree.h"
+
+Tree::Tree()
+{
+
+}
+
+Tree::~Tree()
+{
+
+}
+
+Tree::Tree(QString nameParam, int xParam, int yParam)
+{
+    setSprite(QPixmap(":/sprites/tree.png"));
+    setName(nameParam);
+    setX(xParam);
+    setY(yParam);
+    setPos(x, y);
+}
